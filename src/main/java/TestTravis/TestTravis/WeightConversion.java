b6e5aaffde3678dd21b6011bbc6d8f892a8da63f
Kyle <kyle.taggard96@gmail.com>
2018-02-26 14:49:35 -0800

    First Commit

---
+++ src/main/java/TestTravis/TestTravis/WeightConversion.java
@@ -0,0 +1,14 @@
+package TestTravis.TestTravis;
+
+public class WeightConversion {
+
+	public double KilosToPounds (double kilos){
+		
+		return (kilos / 0.454);
+	}
+	
+public double PoundsToKilos (double pounds){
+		
+		return (pounds * 0.454);
+	}
+}
